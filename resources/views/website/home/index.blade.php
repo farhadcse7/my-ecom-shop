4465960a8355f0182601d0626fdafb71a6dde005
farhadcse7 <fmfarhad23@gmail.com>
2024-09-12 04:52:03 +0600

    website index page mastering updated

--- resources/views/website/home/index.blade.php
+++ resources/views/website/home/index.blade.php
@@ -1,1 +1,1 @@
-<?php
+@extends('website.master')
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebsiteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Website Route list
+Route::get('/', [WebsiteController::class, 'index'])->name('home');
